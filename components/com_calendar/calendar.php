ed8dfdaaef0aebfcefa3df5d7f75104f28a30a73
tapuz <thierry.duhameeuw@gmail.com>
2019-12-11 17:34:15 +0100

    updated copy

--- components/com_calendar/calendar.php
+++ components/com_calendar/calendar.php
@@ -294,6 +294,9 @@ switch (getView())
 		$query = sprintf("SELECT *  FROM table_treatments WHERE practitioner = %s AND scheduled_date BETWEEN '2019-01-01' AND '2025-01-01'",$user);
 
 		$treatments = $wpdb->get_results($query);
+
+		var delta;
+		if($user == 1){delta='+15 minutes'}else {delta='+10 minutes'};
 		
 		foreach($treatments as $treatment){
 			
@@ -302,7 +305,7 @@ switch (getView())
 			$sql = "INSERT INTO table_appointments (user,start,end,patient_id,service,clinic,note) VALUES (%d,%s,%s,%d,%d,%d,%s)";
 			$user = 1;
 			$start = $treatment->scheduled_date . ' ' . $treatment->scheduled_time;
-			$end = date('Y-m-d H:i:s',strtotime('+15 minutes',strtotime($start)));
+			$end = date('Y-m-d H:i:s',strtotime(delta,strtotime($start)));
 			$patient_id = $treatment->patient_id;
 			if ($treatment->type == 1) {$service = 2;}
 			if ($treatment->type == 2) {$service = 3;}
