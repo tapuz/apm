67bb1a9bf1bffcf95a9e4f96b7b27397db418eb0
Thierry <thierry.duhameeuw@gmail.com>
2025-12-21 18:38:33 +0100

    update php 8

--- components/com_invoice/views/edit_invoice.php
+++ components/com_invoice/views/edit_invoice.php
@@ -1,177 +1,175 @@
-<?
+<?php
 /**
  * Edit Invoice
- * 
- * 
- **/ 
-
+ */
 ?>
 
 <script>
-
-var pageTitle = '<?=$pageTitle?>';
-
+var pageTitle = <?= json_encode($pageTitle ?? '', JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) ?>;
 </script>
 
-<input type="hidden" id="invoice_id" value="<?=$invoice->invoice_id?>">
-<input type="hidden" id="invoice_heading" value="<?=$invoice_heading->clinic_invoice_heading;?>">
+<input type="hidden" id="invoice_id" value="<?= htmlspecialchars((string)($invoice->invoice_id ?? ''), ENT_QUOTES, 'UTF-8') ?>">
+<input type="hidden" id="invoice_heading" value="<?= htmlspecialchars((string)($invoice_heading->clinic_invoice_heading ?? ''), ENT_QUOTES, 'UTF-8') ?>">
+
 <div class="col-md-1"></div>
 <div class="col-sm-12 col-md-9" id="invoice"><!-- Start Left content -->
-	
-	
-<div class="row">
-	
-<!--	<a class="btn btn-primary pull-right" a href="index.php?com=invoice&view=list&patient_id=<?=$patient->patient_id?>">Close</a>
--->
-	
-	
-</div>
 
+  <div class="row"></div>
 
+  <div class="row">
+    <div class="box" id="">
+      <div class="box-header hidden-print">
+        <h2><i class="icon-edit"></i>Invoice for: <?= htmlspecialchars((string)($patientName ?? ''), ENT_QUOTES, 'UTF-8') ?></h2>
+      </div>
 
-<div class="row">
-  <div class="box" id="">
-  <div class="box-header hidden-print">
-				<h2><i class="icon-edit"></i>Invoice for: <?php echo $patientName?></h2>
-			</div>
-  <div class="box-content">
- 		
- 	
-  	<div class="row">
-  		
-  		<div class="input-group col-sm-3">
-  			
-  				<label for="date">Datum:</label>	
-	 			<input type="text" class="form-control date-picker" id="date" data-date-format="dd/mm/yyyy" value="<?=$invoice_date->format('d-m-Y');?>"/>
-				
-				<label class="control-label" for="note">Note</label>
-				<input type="text" class="form-control invoiceField" name="note" id="note" placeholder="Invoice note" value="<?=$invoice->note?>" />
-  				
-  			
-  		
-  		</div>
-  		<div class="col-sm-6 print_width_30">
-				<label for="invoice_address">Ontvangen van:</label>
-    
-				<div class="well" contenteditable="true" id="invoice_address" rows="5">
-					<?=$invoice->address?>
-				</div>
-		</div>
-  		
-  		<div class="col-sm-2 pull-right">
-		  <div class="row">
-			<button onclick="printInvoice();" type="button" class="btn btn-primary">Print</button>
-			<button  type="button" class="btn btn-primary close_window">Close</button>
-		  </div>
-		 
-		</div>	
-  	
-  	
-  	
-  	</div>
-  	
-	
-<div class="hidden-print">&nbsp;</div>
+      <div class="box-content">
+        <div class="row">
 
-<div class="hidden-print">&nbsp;</div>
-<div class="row">
-	<div class="col-md-6">
-		<div class="box">
-			<div class="box-header hidden-print">
-				<h2><i class="icon-edit"></i>Invoice Items</h2>
-			</div>
-			<div class="box-content">
-				
-				 
-					<table class="table" id="invoice_items">
-						<thead>
-							<tr>
-								<th class="col-sm-5 print_width_50">Item</th>
-								<th class="col-sm-3 print_width_30">Honorarium</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr><td><td colspan="3" class="sum_invoice hidden-print"><div>Totaal:</div> <div id="sum_invoice"></div></td></tr>
-							<?foreach($invoice_items as $invoice_item){?>
-							<tr id="<?=$invoice_item->invoice_item_id?>"> 
-								<td><?=$invoice_item->item_description?></td> 
-								<td><div class="input-prepend input-group"> 
-										<span class="input-group-addon">€</span> 
-										<input id="appendedPrependedInput" class="form-control item_price" size="16" type="text" value="<?=$invoice_item->item_price?>"> 
-									  </div></td>
-								<td><a class="btn btn-danger delete_invoice_item glyphicon glyphicon-remove" invoice_item_id="<?=$invoice_item->invoice_item_id?>" payment_id="<?=$invoice_item->payment_id?>"></a></td>
-							</tr>
-							
-							<?}?>
-							<tr><td class="visible-print text-right"><strong>Totaal: €</strong></td><td class="visible-print"><strong><div id="sum_invoice_print"></div><strong></td></strong><td>&nbsp</td></tr>
-						</tbody>
-					</table>
-				
-			</div>
-		</div>
-	
-		
-	</div><!--/col -->					
+          <div class="input-group col-sm-3">
+            <label for="date">Datum:</label>
+            <input
+              type="text"
+              class="form-control date-picker"
+              id="date"
+              data-date-format="dd/mm/yyyy"
+              value="<?= htmlspecialchars(($invoice_date instanceof DateTimeInterface) ? $invoice_date->format('d-m-Y') : '', ENT_QUOTES, 'UTF-8') ?>"
+            />
 
-	<div class="col-md-6 noprint">
-   		<div class="box">
-			<div class="box-header">
-				<h2><i class="icon-reorder"></i><span class="break"></span>Consultations</h2>
-			</div>
-			<div class="box-content">
-				<table class="table" id="payments">
-					<thead>
-						<tr>
-							<th>Date</th>
-							<th>Amount</th>
-						</tr>
-					</thead>   
-					<tbody>
-					<?
-					foreach($payments as $payment){
-						?>
-						<tr id="payment_<?=$payment->payment_id?>">
-							<td><?=getDateFromTimestamp($payment->payment_date)?></td>
-							<td><?=$payment->amount?></td>
-							<td><a class="btn btn-primary add_payment_to_invoice" payment_id="<?=$payment->payment_id?>" amount="<?=$payment->amount?>" payment_date="<?=getDateFromTimestamp($payment->payment_date)?>" >Add</a></td>
-						</tr>
-						<?
-					}
-					?>
-					</tbody>
-				</table>
-			</div><!--/box-content -->
-		</div><!--/box --->
-   	</div><!--/col -->
-   	</div><!--/box-content -->
-   	</div><!--/box -->
-   	
-</div><!--/row -->
-</div><!--/row -->
+            <label class="control-label" for="note">Note</label>
+            <input
+              type="text"
+              class="form-control invoiceField"
+              name="note"
+              id="note"
+              placeholder="Invoice note"
+              value="<?= htmlspecialchars((string)($invoice->note ?? ''), ENT_QUOTES, 'UTF-8') ?>"
+            />
+          </div>
 
+          <div class="col-sm-6 print_width_30">
+            <label for="invoice_address">Ontvangen van:</label>
+            <div class="well" contenteditable="true" id="invoice_address" rows="5">
+              <?= $invoice->address ?>
+            </div>
+          </div>
 
-<div class="row visible-print">
-	<?=$signature?>
-</div>
-<div class="row visible-print">
-   <?=$practitioner_name;?>
-   		
-</div>
+          <div class="col-sm-2 pull-right">
+            <div class="row">
+              <button onclick="printInvoice();" type="button" class="btn btn-primary">Print</button>
+              <button type="button" class="btn btn-primary close_window">Close</button>
+            </div>
+          </div>
 
+        </div>
+
+        <div class="hidden-print">&nbsp;</div>
+        <div class="hidden-print">&nbsp;</div>
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="box">
+              <div class="box-header hidden-print">
+                <h2><i class="icon-edit"></i>Invoice Items</h2>
+              </div>
+
+              <div class="box-content">
+                <table class="table" id="invoice_items">
+                  <thead>
+                    <tr>
+                      <th class="col-sm-5 print_width_50">Item</th>
+                      <th class="col-sm-3 print_width_30">Honorarium</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td colspan="2" class="sum_invoice hidden-print">
+                        <div>Totaal:</div> <div id="sum_invoice"></div>
+                      </td>
+                    </tr>
+
+                    <?php foreach (($invoice_items ?? []) as $invoice_item): ?>
+                      <tr id="<?= htmlspecialchars((string)($invoice_item->invoice_item_id ?? ''), ENT_QUOTES, 'UTF-8') ?>">
+                        <td><?= htmlspecialchars((string)($invoice_item->item_description ?? ''), ENT_QUOTES, 'UTF-8') ?></td>
+                        <td>
+                          <div class="input-prepend input-group">
+                            <span class="input-group-addon">€</span>
+                            <input
+                              class="form-control item_price"
+                              size="16"
+                              type="text"
+                              value="<?= htmlspecialchars((string)($invoice_item->item_price ?? ''), ENT_QUOTES, 'UTF-8') ?>"
+                            >
+                          </div>
+                        </td>
+                        <td>
+                          <a
+                            class="btn btn-danger delete_invoice_item glyphicon glyphicon-remove"
+                            invoice_item_id="<?= htmlspecialchars((string)($invoice_item->invoice_item_id ?? ''), ENT_QUOTES, 'UTF-8') ?>"
+                            payment_id="<?= htmlspecialchars((string)($invoice_item->payment_id ?? ''), ENT_QUOTES, 'UTF-8') ?>"
+                          ></a>
+                        </td>
+                      </tr>
+                    <?php endforeach; ?>
+
+                    <tr>
+                      <td class="visible-print text-right"><strong>Totaal: €</strong></td>
+                      <td class="visible-print"><strong><span id="sum_invoice_print"></span></strong></td>
+                      <td>&nbsp;</td>
+                    </tr>
+
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div><!--/col -->
+
+          <div class="col-md-6 noprint">
+            <div class="box">
+              <div class="box-header">
+                <h2><i class="icon-reorder"></i><span class="break"></span>Consultations</h2>
+              </div>
+              <div class="box-content">
+                <table class="table" id="payments">
+                  <thead>
+                    <tr>
+                      <th>Date</th>
+                      <th>Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php foreach (($payments ?? []) as $payment): ?>
+                      <tr id="payment_<?= htmlspecialchars((string)($payment->payment_id ?? ''), ENT_QUOTES, 'UTF-8') ?>">
+                        <td><?= htmlspecialchars((string)getDateFromTimestamp($payment->payment_date ?? null), ENT_QUOTES, 'UTF-8') ?></td>
+                        <td><?= htmlspecialchars((string)($payment->amount ?? ''), ENT_QUOTES, 'UTF-8') ?></td>
+                        <td>
+                          <a
+                            class="btn btn-primary add_payment_to_invoice"
+                            payment_id="<?= htmlspecialchars((string)($payment->payment_id ?? ''), ENT_QUOTES, 'UTF-8') ?>"
+                            amount="<?= htmlspecialchars((string)($payment->amount ?? ''), ENT_QUOTES, 'UTF-8') ?>"
+                            payment_date="<?= htmlspecialchars((string)getDateFromTimestamp($payment->payment_date ?? null), ENT_QUOTES, 'UTF-8') ?>"
+                          >Add</a>
+                        </td>
+                      </tr>
+                    <?php endforeach; ?>
+                  </tbody>
+                </table>
+              </div><!--/box-content -->
+            </div><!--/box -->
+          </div><!--/col -->
+
+        </div><!--/row -->
+
+      </div><!--/box-content -->
+    </div><!--/box -->
+  </div><!--/row -->
+
+  <div class="row visible-print">
+    <?= $signature ?? '' ?>
+  </div>
+  <div class="row visible-print">
+    <?= htmlspecialchars((string)($practitioner_name ?? ''), ENT_QUOTES, 'UTF-8') ?>
+  </div>
 
 </div><!--/col /left content -->
 
-
 <div class="col-md-2"></div>
-<!-- <div class="col-md-3 visible-md visible-lg" id="feed">
- Start Right content 
-		<?//loadModule('patient_menu');?>
-	
-	
-</div> /col /Right Content -->
-	
-</div><!--/row-->
-
- 		
-			
-
--- components/com_invoice/views/list.php
+++ components/com_invoice/views/list.php
@@ -1,91 +1,79 @@
-<?
+<?php
 /**
- * 
- * 
- * 
- **/ 
-
+ *
+ *
+ *
+ **/
 ?>
 
 <script>
-
-var pageTitle = '<?=$pageTitle?>';
-
+    var pageTitle = <?= json_encode($pageTitle ?? '') ?>;
 </script>
+
 <div class="col-sm-12 col-md-9"><!-- Start Left content -->
-	
-	<!-- start: patient_name -->
-	<?loadModule('patient_name');?>
-	
-	<!-- /patient_name-->
-	
-<div class="row">
-	
-	<a type="button" class="btn btn-primary" href="index.php?com=invoice&view=edit_invoice&task=create_new_invoice&patient_id=<?=$patient_id?>">New Invoice</a>
-	
-	
-	
-</div>
-<div class="row">&nbsp;</div>
 
-<div class="row">
-	<div class="col-md-6">
-	       	
-   		<div class="box">
-			<div class="box-header">
-				<h2><i class="icon-reorder"></i><span class="break"></span>Invoices</h2>
-			</div>
-			
-			<div class="box-content">
-        		<table class="table">
-		         <thead>
-		           <tr>
-		           	 <th>ID</th>	
-		             <th>Date</th>
-		             <th>Amount</th>
-		             
-		   		   </tr>
-		         </thead>
-		         <tbody>
-		           
-		           <?foreach ($invoices as $invoice) 
-     				{?>		
-		           <tr id="<?=$invoice->invoice_id?>">	 
-		           	 <td><?=$invoice->invoice_id?></td>
-		           	 <td><?=$invoice->date?></td>
-		             <td><?=$invoice->amount?></td>
-		             <td>
-		             	<a class="btn btn-success" a href="index.php?com=invoice&view=edit_invoice&invoice_id=<?=$invoice->invoice_id?>&patient_id=<?=$patient_id?>">View</a>
-		             	<a class="btn btn-danger delete_invoice" invoice_id="<?=$invoice->invoice_id?>" >Delete</a>
-		             </td>
-		             
-		           </tr>
-        	
-    			
-			    <? }?>
-			    </tbody>
-   			   </table>
-   			</div>
-   		</div>
-   	</div><!--/col -->
-   
-</div><!--/row -->
+    <!-- start: patient_name -->
+    <?php loadModule('patient_name'); ?>
+    <!-- /patient_name-->
 
+    <div class="row">
+        <a type="button"
+           class="btn btn-primary"
+           href="index.php?com=invoice&view=edit_invoice&task=create_new_invoice&patient_id=<?= urlencode((string)($patient_id ?? '')) ?>">
+            New Invoice
+        </a>
+    </div>
 
+    <div class="row">&nbsp;</div>
+
+    <div class="row">
+        <div class="col-md-6">
+
+            <div class="box">
+                <div class="box-header">
+                    <h2><i class="icon-reorder"></i><span class="break"></span>Invoices</h2>
+                </div>
+
+                <div class="box-content">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Date</th>
+                            <th>Amount</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        <?php foreach (($invoices ?? []) as $invoice): ?>
+                            <tr id="<?= htmlspecialchars((string)$invoice->invoice_id, ENT_QUOTES, 'UTF-8') ?>">
+                                <td><?= htmlspecialchars((string)$invoice->invoice_id, ENT_QUOTES, 'UTF-8') ?></td>
+                                <td><?= htmlspecialchars((string)$invoice->date, ENT_QUOTES, 'UTF-8') ?></td>
+                                <td><?= htmlspecialchars((string)$invoice->amount, ENT_QUOTES, 'UTF-8') ?></td>
+                                <td>
+                                    <a class="btn btn-success"
+                                       href="index.php?com=invoice&view=edit_invoice&invoice_id=<?= urlencode((string)$invoice->invoice_id) ?>&patient_id=<?= urlencode((string)($patient_id ?? '')) ?>">
+                                        View
+                                    </a>
+
+                                    <a class="btn btn-danger delete_invoice"
+                                       data-invoice-id="<?= htmlspecialchars((string)$invoice->invoice_id, ENT_QUOTES, 'UTF-8') ?>">
+                                        Delete
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div><!--/col -->
+    </div><!--/row -->
 
 </div><!--/col /left content -->
 
 <div class="col-md-3 visible-md visible-lg" id="feed"><!-- Start Right content -->
-		<?loadModule('patient_menu');?>
-	
+    <?php loadModule('patient_menu'); ?>
 </div><!--/col /Right Content-->
-	
-</div><!--/row-->
-
- 		
-			
-
-				
-
-
-
